5e138c343ad162f139475fa9da850c6ec70fc10f
ombasa <ombasaherman@gmail.com>
2023-04-28 17:46:26 +0300

    create task

--- todoApp/tasks/views.py
+++ todoApp/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . models import *
 from .forms import *
@@ -8,6 +8,12 @@ from .forms import *
 def index(request):
     tasks = Task.objects.all()
     form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
 
     context = {'tasks':tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
