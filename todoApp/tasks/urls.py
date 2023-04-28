7c30461ffb74ceb8f896518b83fd873b8776cabf
ombasa <ombasaherman@gmail.com>
2023-04-28 23:36:02 +0300

    delete task

--- todoApp/tasks/urls.py
+++ todoApp/tasks/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='list'),
     path('update/<str:pk>/', views.updateTask, name='update'),
+    path('delete/<str:pk>/', views.deleteTask, name='delete'),
 ]
