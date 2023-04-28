6958f54788a808750e255d738a55ee34bf9b68b3
ombasa <ombasaherman@gmail.com>
2023-04-28 19:00:27 +0300

    update task

--- todoApp/tasks/urls.py
+++ todoApp/tasks/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='list'),
+    path('update/<str:pk>/', views.updateTask, name='update'),
 ]
