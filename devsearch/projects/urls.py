9d5555e518ec2aef4acbf0a3f3e2be5851a8a54b
ombasa <ombasaherman@gmail.com>
2023-05-01 14:29:05 +0300

    CRUD functionality

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="project"),
+
+    path('create-project/', views.createProject, name="create-project"),
+    path('delete-project/<str:pk>/', views.deleteProject, name="delete-project"),
+    path('update-project/<str:pk>/', views.updateProject, name="update-project"),
+
+
 ]
